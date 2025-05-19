774e9c4d82b6ec1ed257f91a429fbf9f38381522
balumahendranv <43694011+balumahendranv@users.noreply.github.com>
2025-05-19 19:28:19 +0530

    Update submit.php

--- docker/apache-php/submit.php
+++ docker/apache-php/submit.php
@@ -1,11 +1,22 @@
-$mysqli = new mysqli(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_NAME"));
+<?php
 $subdomain = preg_replace('/[^a-z0-9\-]/', '', strtolower($_POST['subdomain']));
-$title = $_POST['title'];
-$content = $_POST['content'];
+$title = htmlspecialchars($_POST['title']);
+$content = htmlspecialchars($_POST['content']);
 
-$stmt = $mysqli->prepare("INSERT INTO pages (subdomain, title, content) VALUES (?, ?, ?)");
+// Save to database
+$conn = new mysqli("localhost", "user", "password", "yourdb");
+$stmt = $conn->prepare("INSERT INTO pages (subdomain, title, content) VALUES (?, ?, ?)");
 $stmt->bind_param("sss", $subdomain, $title, $content);
 $stmt->execute();
+$stmt->close();
+$conn->close();
 
-file_put_contents("pages/{$subdomain}.php", "<h1>$title</h1><p>$content</p>");
-header("Location: http://{$subdomain}.rootedinfra.com/pages/{$subdomain}.php");
+// Create directory and file
+$dir = "/var/www/vhosts/$subdomain";
+if (!file_exists($dir)) {
+    mkdir($dir, 0755, true);
+    $html = "<h1>$title</h1><p>$content</p>";
+    file_put_contents("$dir/index.php", $html);
+}
+?>
+Page created! Access it at: http://<?php echo $subdomain; ?>.rootedinfra.com
